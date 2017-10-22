2c0b7a5d898de1933913c8b1534b69e0ea879efd
Mrinal Pandey <mrinalpandey6@gmail.com>
2017-10-22 16:10:52 +0530

    new
    new

--- README.md
+++ README.md
@@ -4,17 +4,21 @@ The various stages of the projects have tests of their own that ensure that ther
 
 The tests are:
 
-1. The first test is a basic test on RSS feeds that ensures-
-	a) all RSS feeds are defined and are not empty
-	b) all the feeds are defined and have URL
-	c) all the feeds are defined and have name
+## The first test is a basic test on RSS feeds that ensures-
 
-2. The second test is a test for the menu button which checks that-
-	a) the menu element is hidden by default
-	b) the menu shows when clicked and hides on double click
+* all RSS feeds are defined and are not empty
+* all the feeds are defined and have URL
+* all the feeds are defined and have name
 
-3. The third test is a test for the initial entries on the page that checks whether-
-	a) the feed has at least one entry when loaded
+## The second test is a test for the menu button which checks that-
 
-4. The fourth test is a test on the selection of new feed that ensures-
-	a) all the entries/feeds are unique
+* the menu element is hidden by default
+* the menu shows when clicked and hides on double click
+
+## The third test is a test for the initial entries on the page that checks whether-
+
+* the feed has at least one entry when loaded
+
+## The fourth test is a test on the selection of new feed that ensures-
+
+* all the entries/feeds are unique
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -15,12 +15,12 @@ $(function() {
     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        * allFeeds variable has been defined and that it is not
+        * empty. Experiment with this before you get started on
+        * the rest of this project. What happens when you change
+        * allFeeds in app.js to be an empty array and refresh the
+        * page?
+        */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -28,9 +28,9 @@ $(function() {
 
 
         /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        * in the allFeeds object and ensures it has a URL defined
+        * and that the URL is not empty.
+        */
 
         it('has URL', function() {
             allFeeds.forEach(function(val){
@@ -39,9 +39,9 @@ $(function() {
            });
         });
         /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        * in the allFeeds object and ensures it has a name defined
+        * and that the name is not empty.
+        */
 
         it('has name', function() {
             allFeeds.forEach(function(val){
@@ -58,27 +58,33 @@ $(function() {
         var menuIcon = $('.menu-icon-link');
         var className;
         /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        * hidden by default. You'll have to analyze the HTML and
+        * the CSS to determine how we're performing the
+        * hiding/showing of the menu element.
+        */
         it('shows on click', function() {
             className = $('body').attr('class').split(' ');
 
             expect(className).toContain('menu-hidden');
         });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-          it('shows/hides menu on click', function() {
+        
+        /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+        it('shows/hides menu on click', function() {
             $('.menu-icon-link').click();
 
             className = $('body').attr('class').split(' ');
             expect(className).not.toContain('menu-hidden');
 
-            $('a[data-id="1"]').click();
+            $('.menu-icon-link').click();
+
+            className = $('body').attr('class').split(' ');
+            expect(className).toContain('menu-hidden');
+
+            /*$('a[data-id="1"]').click();
 
             className = $('body').attr('class').split(' ');
             expect(className).toContain('menu-hidden');
@@ -91,9 +97,10 @@ $(function() {
             $('a[data-id="3"]').click();
 
             className = $('body').attr('class').split(' ');
-            expect(className).toContain('menu-hidden');
-          });
+            expect(className).toContain('menu-hidden');*/
+        });
     });
+
     /* TODO: Write a new test suite named "Initial Entries" */
     describe("Initial Entries", function() {
         // var LoadFeed = new loadFeed();
@@ -108,11 +115,12 @@ $(function() {
             
         });
 
-        it("are not empty", function(done) {
-            expect($('.entry-link').length).not.toBe(0);
-            done();
+        it("are not empty", function() {
+            expect($('.feed .entry-link')).not.toBe(null);
+            
         });
     });
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe("New Feed Selection", function() {
         /* TODO: Write a test that ensures when a new feed is loaded
@@ -122,15 +130,16 @@ $(function() {
         var firstFeed, secondFeed;
 
         beforeEach(function(done) {
-            loadFeed(0, function() {
+            loadFeed(0, function(done) {
                 firstFeed = $('.feed').html();
             });
+            loadFeed(0, function(done) {
+                secondFeed = $('.feed').html();
+            })
         });
 
-        it("two different links", function() {
-            secondFeed = $('.feed').html();
+        it("two different links", function(done) {
             expect(firstFeed).not.toBe(secondFeed);
-            done();
         });
     });
 });
